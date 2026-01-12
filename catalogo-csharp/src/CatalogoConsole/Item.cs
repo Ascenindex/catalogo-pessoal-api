b53126f5dbafc139213cb7c8d7d58b4d7afb3426
= <ascendinomartins2303@gmail.com>
2026-01-12 09:45:21 -0300

    Dia 1: Cria classe Item com propriedades Id, Nome, Status

---
+++ catalogo-csharp/src/CatalogoConsole/Item.cs
@@ -0,0 +1,15 @@
+﻿namespace DefaultNamespace;
+
+public class Item
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public bool Status { get; set; }
+
+    public Item(int id, string name, bool status)
+    {
+        this.Id = id;
+        this.Name = name;
+        this.Status = status;
+    }
+}
